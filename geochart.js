179e611159945316148b615f083a02ca10005cb1
PineapplePraise <peanutbutterfoam@gmail.com>
2021-09-05 13:36:36 -0700

    interactive map

--- geochart.js
+++ geochart.js
@@ -1,33 +1,22 @@
 google.charts.load('current', {
-    'packages': ['geochart'],
-    // Note: Because markers require geocoding, you'll need a mapsApiKey.
-    // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
-    'mapsApiKey': 'AIzaSyAGcoQcq1r34xYJsRnnl9solXHofLprRPk'
-});
-google.charts.setOnLoadCallback(drawMarkersMap);
+    'packages':['geochart'],
+  });
+  google.charts.setOnLoadCallback(drawRegionsMap);
 
-function drawMarkersMap() {
+  function drawRegionsMap() {
     var data = google.visualization.arrayToDataTable([
-        ['City',   'Population', 'Area'],
-        ['Rome',      2761477,    1285.31],
-        ['Milan',     1324110,    181.76],
-        ['Naples',    959574,     117.27],
-        ['Turin',     907563,     130.17],
-        ['Palermo',   655875,     158.9],
-        ['Genoa',     607906,     243.60],
-        ['Bologna',   380181,     140.7],
-        ['Florence',  371282,     102.41],
-        ['Fiumicino', 67370,      213.44],
-        ['Anzio',     52192,      43.43],
-        ['Ciampino',  38262,      11]
+      ['Country', 'Popularity'],
+      ['Germany', 200],
+      ['United States', 300],
+      ['Brazil', 400],
+      ['Canada', 500],
+      ['France', 600],
+      ['RU', 700]
     ]);
 
-    var options = {
-        region: 'IT',
-        displayMode: 'markers',
-        colorAxis: {colors: ['green', 'blue']}
-    };
+    var options = {};
 
-    var chart = new google.visualization.GeoChart(document.getElementById('chart_div'));
+    var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+
     chart.draw(data, options);
-};
+  }
