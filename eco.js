669b06858377b2aad6a12a71327e8391922e895d
PineapplePraise <peanutbutterfoam@gmail.com>
2021-09-05 21:39:29 -0700

    Michael helped alot

--- eco.js
+++ eco.js
@@ -1,14 +1,1 @@
-info = document.getElementById('info')
-var sty = info.style
-sty.visibility = "hidden"
-
-function infoClick(){
-    if (sty.visibility == "hidden") {
-        sty.visibility = "visible"
-    } else {
-        sty.visibility = "hidden"
-    }
-}
-
-
 
--- geochart.js
+++ geochart.js
@@ -1,3 +1,110 @@
+// follow the format/template, don't forget to put comment
+//https://github.com/emrahonder/All-Countries-For-Google-GeoChart/blob/master/geochart-country-name.html
+
+var species = [
+  {
+    "animal": "rhinos",
+    "countries": [
+        {
+            "country": "South Africa",
+            "population": 16000
+        },
+        {
+            "country": "Zimbabwe",
+            "population": 1000
+        },
+        {
+            "country": "Kenya",
+            "population": 1258
+        }
+    ]
+  },
+  {
+    "animal": "Sea Turtles",
+    "countries": [
+        {
+            "country": "Costa Rica",
+            "population": 550000
+        },
+        {
+            "country": "Mexico",
+            "population": 450000
+        },
+        {
+            "country": "India",
+            "population": 135000
+        }
+    ]
+  },
+  {
+    "animal": "Sea Turtles",
+    "countries": [
+        {
+            "country": "Costa Rica",
+            "population": 550000
+        },
+        {
+            "country": "Mexico",
+            "population": 450000
+        },
+        {
+            "country": "India",
+            "population": 135000
+        }
+    ]
+  },
+  {
+    "animal": "Sea Turtles",
+    "countries": [
+        {
+            "country": "Costa Rica",
+            "population": 550000
+        },
+        {
+            "country": "Mexico",
+            "population": 450000
+        },
+        {
+            "country": "India",
+            "population": 135000
+        }
+    ]
+  },
+  {
+    "animal": "Sea Turtles",
+    "countries": [
+        {
+            "country": "Costa Rica",
+            "population": 550000
+        },
+        {
+            "country": "Mexico",
+            "population": 450000
+        },
+        {
+            "country": "India",
+            "population": 135000
+        }
+    ]
+  }
+]
+
+document.getElementById("list").innerHTML = "";
+
+for (var i = 0; i < species.length; ++i) {
+    document.getElementById("list").innerHTML += "<a href='#' class='link-item'>" + species[i].animal + "</a>";
+}
+
+// for (i = 0; i < species.length; i++) {
+//   var node = document.createElement("a");
+//   var space = document.createTextNode(" ");
+//   node.innerHTML = species[i].animal;
+//   node.onclick = "drawRegionsMap(" + i + ")"; //drawRegionsMap( value of i );
+//   node.onclick = ;
+//   document.getElementById("list").appendChild(node);
+//   document.getElementById("list").appendChild(space);
+// }
+
 google.charts.load('current', {
     'packages':['geochart'],
   });
@@ -5,14 +112,18 @@ google.charts.load('current', {
 
   function drawRegionsMap() {
     var data = google.visualization.arrayToDataTable([
-      ['Country', 'Popularity'],
-      ['Germany', 200],
-      ['United States', 300],
-      ['Brazil', 400],
-      ['Canada', 500],
-      ['France', 600],
-      ['Russia', 700]
+      ['Country', 'Population'],
+      ['United States', 100]
+    //   ['Germany', 200],
+    //   ['United States', 300],
+    //   ['Brazil', 400],
+    //   ['Canada', 500],
+    //   ['France', 600],
+    //   ['Russia', 700],
+    //   ['Australia', 600]
     ]);
+
+    
 
     var options = {};
 
