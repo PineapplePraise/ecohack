8f671104ebe2fe4faf3b27bd160c4084f33a9385
PineapplePraise <peanutbutterfoam@gmail.com>
2021-09-06 11:02:20 -0700

    info

--- geochart.js
+++ geochart.js
@@ -18,19 +18,20 @@ var species = [
             "population": 1258
         }
     ],
-    "img":"https://files.worldwildlife.org/wwfcmsprod/images/Mountain_Gorilla_Silverback_WW22557/hero_small/4vzzw2piv9_Mountain_Gorilla_Silverback_WW22557.jpg",
+    "img":"https://www.treehugger.com/thmb/svjomF7zBTemEeUw-5IJnmcJpkU=/3425x2569/smart/filters:no_upscale()/black-rhinocerous-in-open-field-10181997-69cb737c12e04535b45c3c8324632df3.jpg",
     //find a good image of species on web, right click, 'copy image link' or 'open image in new tab', make sure its just an image and black background, not a site and paste link here
     "info":[
-        "https://en.wikipedia.org/wiki/Gorilla",
-        "https://www.worldwildlife.org/species/gorilla",
-        "https://www.britannica.com/animal/Gorilla-primate-genus",
+        "https://en.wikipedia.org/wiki/Rhinoceros",
+        "https://www.britannica.com/animal/rhinoceros-mammal",
+        "https://www.worldwildlife.org/species/rhino",
+
     ],
     "donate":[
-        "https://gorillafund.org/",
-        "https://gorillafund.org/get-involved/",
-        "https://www.gorillas.org/donate/",
-        "https://gifts.worldwildlife.org/gift-center/gifts/species-adoptions/gorilla.aspx",
-        "https://gracegorillas.org/grace-save-a-gorilla/"
+        "https://www.savetherhino.org/get-involved/donate/",
+        "https://rhinos.org/donate/",
+        "https://www.helpingrhinos.org/donate/"
+
+
     ]
     //learn more/research and donate links. if its taking over 2 minutes to get this many links, you are probably not doing it right and please let me know. it shouldn't be difficult.
   },
@@ -50,11 +51,17 @@ var species = [
             "population": 135000
         }
     ],
-    "img":"https://files.worldwildlife.org/wwfcmsprod/images/Mountain_Gorilla_Silverback_WW22557/hero_small/4vzzw2piv9_Mountain_Gorilla_Silverback_WW22557.jpg",
+    "img":"https://files.worldwildlife.org/wwfcmsprod/images/Sea_Turtle_Hol_Chan_Marine_Reserve_WW1105958/story_full_width/11e3bxo822_Sea_Turtle_Hol_Chan_Marine_Reserve_WW1105958.jpg",
     "info":[
+        "https://en.wikipedia.org/wiki/Sea_turtle",
+        "https://www.britannica.com/animal/sea-turtle",
+        "https://www.worldwildlife.org/species/sea-turtle",
 
     ],
     "donate":[
+        "https://www.shop-marinelife.org/adoptions/",
+        "https://saveturtles.org/donate-save-sea-turtles-conservation-protect-endagered-species-charity-guide-save-marine-life-protect-sea-turtles/"
+        
 
     ]
   },
@@ -67,11 +74,16 @@ var species = [
         }
 
     ],
-    "img":"https://files.worldwildlife.org/wwfcmsprod/images/Mountain_Gorilla_Silverback_WW22557/hero_small/4vzzw2piv9_Mountain_Gorilla_Silverback_WW22557.jpg",
+    "img":"https://s28164.pcdn.co/files/Berani5.jpg",
     "info":[
+        "https://en.wikipedia.org/wiki/Malayan_tiger",
+        "https://www.britannica.com/animal/Malayan-tiger",
+        
         
     ],
     "donate":[
+        "https://www.globalgiving.org/projects/protect-the-malayan-tiger-and-restore-its-habitat/donate/",
+        "https://www.wwf.org.my/how_you_can_help/donate_now/save_our_malayan_tigers/"
 
     ]
   },
@@ -79,15 +91,185 @@ var species = [
     "animal": "Asian Elephant",
     "countries": [
         {
-            "country": "Costa Rica",
-            "population": 550000
-        }
+            "country": "Myanmar",
+            "population": 3000
+        },
+        {
+            "country": "Thailand",
+            "population": 3000
+        },
+        {
+            "country": "Cambodia",
+            "population": 400
+        },
+        {
+            "country": "Laos",
+            "population": 1000
+        },
+        {
+            "country": "India",
+            "population": 550000 
+        },
+        {
+            "country": "Vietnam",
+            "population": 90
+        },
+        {
+            "country": "Malaysia",
+            "population": 1300
+        },
+        {
+            "country": "Indonesia",
+            "population": 1100
+        },
+        {
+            "country": "Bangladesh",
+            "population": 200
+        },
     ],
-    "img":"https://files.worldwildlife.org/wwfcmsprod/images/Mountain_Gorilla_Silverback_WW22557/hero_small/4vzzw2piv9_Mountain_Gorilla_Silverback_WW22557.jpg",
+    "img":"",
     "info":[
+        "https://www.worldwildlife.org/species/asian-elephant",
+        "https://www.britannica.com/animal/Asian-elephant",
+        "https://en.wikipedia.org/wiki/Asian_elephant",
         
     ],
     "donate":[
+        "https://www.asianelephantsupport.org/donate",
+        "https://www.globalgiving.org/projects/elephant-rescue-asia/"
+
+    ]
+  },
+  {
+    "animal": "Snow Leopard",
+    "countries": [
+        {
+            "country": "Malaysia",
+            "population": 200
+        },
+        {
+            "country": "Malaysia",
+            "population": 200
+        }
+
+    ],
+    "img":"https://i.guim.co.uk/img/media/c356ea76561ab0e0631ad7b532dc4ba81e66fef7/0_146_5131_3079/master/5131.jpg?width=620&quality=85&auto=format&fit=max&s=231fcd81b3c3cfa9922f4998c7a01a92",
+    "info":[
+        "https://en.wikipedia.org/wiki/Snow_leopard",
+        "https://www.worldwildlife.org/species/snow-leopard",
+        "https://www.nationalgeographic.com/animals/mammals/facts/snow-leopard"
+    ],
+    "donate":[
+        "https://snowleopard.org/",
+        "https://gifts.worldwildlife.org/gift-center/gifts/species-adoptions/snow-leopard.aspx"
+    ]
+  },
+  {
+    "animal": "Saola",
+    "countries": [
+        {
+            "country": "Vietnam",
+            "population": 250
+        }
+
+    ],
+    "img":"https://wwfint.awsassets.panda.org/img/saola_dhulse47098_352248.jpg",
+    "info":[
+        "https://en.wikipedia.org/wiki/Saola#:~:text=The%20saola%20(Pseudoryx%20nghetinhensis)%2C,Range%20of%20Vietnam%20and%20Laos.",
+        "https://www.worldwildlife.org/species/saola",
+        
+        
+    ],
+    "donate":[
+        "https://www.savethesaola.org/",
+
+    ]
+  },
+  {
+    "animal": "Malayan Tiger",
+    "countries": [
+        {
+            "country": "Malaysia",
+            "population": 200
+        }
+
+    ],
+    "img":"https://s28164.pcdn.co/files/Berani5.jpg",
+    "info":[
+        "https://en.wikipedia.org/wiki/Malayan_tiger",
+        "https://www.britannica.com/animal/Malayan-tiger",
+        
+        
+    ],
+    "donate":[
+        "https://www.globalgiving.org/projects/protect-the-malayan-tiger-and-restore-its-habitat/donate/",
+        "https://www.wwf.org.my/how_you_can_help/donate_now/save_our_malayan_tigers/"
+
+    ]
+  },
+  {
+    "animal": "Malayan Tiger",
+    "countries": [
+        {
+            "country": "Malaysia",
+            "population": 200
+        }
+
+    ],
+    "img":"https://s28164.pcdn.co/files/Berani5.jpg",
+    "info":[
+        "https://en.wikipedia.org/wiki/Malayan_tiger",
+        "https://www.britannica.com/animal/Malayan-tiger",
+        
+        
+    ],
+    "donate":[
+        "https://www.globalgiving.org/projects/protect-the-malayan-tiger-and-restore-its-habitat/donate/",
+        "https://www.wwf.org.my/how_you_can_help/donate_now/save_our_malayan_tigers/"
+
+    ]
+  },
+  {
+    "animal": "Malayan Tiger",
+    "countries": [
+        {
+            "country": "Malaysia",
+            "population": 200
+        }
+
+    ],
+    "img":"https://s28164.pcdn.co/files/Berani5.jpg",
+    "info":[
+        "https://en.wikipedia.org/wiki/Malayan_tiger",
+        "https://www.britannica.com/animal/Malayan-tiger",
+        
+        
+    ],
+    "donate":[
+        "https://www.globalgiving.org/projects/protect-the-malayan-tiger-and-restore-its-habitat/donate/",
+        "https://www.wwf.org.my/how_you_can_help/donate_now/save_our_malayan_tigers/"
+
+    ]
+  },
+  {
+    "animal": "Malayan Tiger",
+    "countries": [
+        {
+            "country": "Malaysia",
+            "population": 200
+        }
+
+    ],
+    "img":"https://s28164.pcdn.co/files/Berani5.jpg",
+    "info":[
+        "https://en.wikipedia.org/wiki/Malayan_tiger",
+        "https://www.britannica.com/animal/Malayan-tiger",
+        
+        
+    ],
+    "donate":[
+        "https://www.globalgiving.org/projects/protect-the-malayan-tiger-and-restore-its-habitat/donate/",
+        "https://www.wwf.org.my/how_you_can_help/donate_now/save_our_malayan_tigers/"
 
     ]
   },
@@ -101,20 +283,30 @@ var species = [
         }
     ],
     "img":"https://files.worldwildlife.org/wwfcmsprod/images/Mountain_Gorilla_Silverback_WW22557/hero_small/4vzzw2piv9_Mountain_Gorilla_Silverback_WW22557.jpg",
+    //find a good image of species on web, right click, 'copy image link' or 'open image in new tab', make sure its just an image and black background, not a site and paste link here
     "info":[
-        
+        "https://en.wikipedia.org/wiki/Gorilla",
+        "https://www.worldwildlife.org/species/gorilla",
+        "https://www.britannica.com/animal/Gorilla-primate-genus",
     ],
     "donate":[
-
+        "https://gorillafund.org/get-involved/",
+        "https://www.gorillas.org/donate/",
+        "https://gifts.worldwildlife.org/gift-center/gifts/species-adoptions/gorilla.aspx",
+        "https://gracegorillas.org/grace-save-a-gorilla/"
     ]
-  },
+  }
   
 ]
 
 document.getElementById("list").innerHTML = "";
 
+
+
 for (var i = 0; i < species.length; ++i) {
     document.getElementById("list").innerHTML += "<a href='javascript:drawSpecies(" + i + ")' class='link-item'>" + species[i].animal + "</a>";
+    console.log(species[i].info)
+    document.getElementById("info").innerHTML = "Research links \n" + species[i].info
 }
 
 google.charts.load('current', {
@@ -151,14 +343,14 @@ google.charts.load('current', {
     var pop = [
         ['Country', 'Population']
     ];
-
+    
     for (var i=0; i<species[animal].countries.length; i++){
         pop.push([species[animal].countries[i].country, species[animal].countries[i].population]);
     }
 
+    
     var data = google.visualization.arrayToDataTable(pop);
     var options = {};
     var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
     chart.draw(data, options);
-    
   }
